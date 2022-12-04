db3382095894341846dc0d7a6c329fb809a07ee2
Lev <Kakalashvilillev@yandex.ru>
2022-12-04 18:53:10 +0300

    refactor: rename app config

--- app/db.py
+++ app/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
-from app.settings import Config as app_config
+
+from app.settings import Config as AppConfig
 from sqlalchemy.orm import Session
 
-db_engine = create_engine(app_config.DB_URL, echo=True if app_config.DEBUG == "True" else False)
+db_engine = create_engine(AppConfig.DB_URL, echo=True if AppConfig.DEBUG == "True" else False)
 
 session = Session(db_engine)
