7492b187b4efa040fcb9b16a293748cce36f7fa9
Lev <Kakalashvilillev@yandex.ru>
2022-12-10 14:46:49 +0300

    add: - переделал проверку username и password в теле запроса - добавил класс для валидации username и password

--- app/validators.py
+++ app/validators.py
@@ -1,8 +1,28 @@
+from decimal import Decimal
+
 from sanic import request as request_type
+from pydantic import BaseModel, ValidationError
 
 
-def check_credentials(request: request_type) -> bool:
-    if getattr(request, "credentials") is not None \
-            and (request.credentials.username and request.credentials.password):
-        return True
-    return False
+class TransactionData(BaseModel):
+    signature: str
+    transaction_id: int
+    user_id: int
+    bill_id: int
+    amount: Decimal
+
+
+class UserData(BaseModel):
+    username: str
+    password: str
+
+
+def check_userdata(request: request_type) -> UserData | None:
+    try:
+        return UserData(**request.json)
+    except ValidationError:
+        return None
+
+
+def check_transaction_data(request: request_type) -> TransactionData | None:
+    return TransactionData(**request.json)
