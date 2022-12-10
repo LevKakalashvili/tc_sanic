3e6fef868f1cd02f0ea2bf00632fa3f1c79eab08
Lev <Kakalashvilillev@yandex.ru>
2022-12-10 14:47:55 +0300

    chore: переписал проверку пользовательских данных

--- app/handlers.py
+++ app/handlers.py
@@ -1,13 +1,14 @@
 import json
 from uuid import UUID
 
-from sanic import exceptions, json, redirect
+from sanic import json
 from sanic.views import HTTPMethodView
+from sanic_jwt import protected
 from sqlalchemy import select
 
 from app.db import session as db_session
 from app.models import Good, User, InactiveUser
-from app.validators import check_credentials
+from app.validators import check_userdata, check_transaction_data
 from app.urls import Urls
 
 
@@ -21,15 +22,15 @@ class UserActivation(HTTPMethodView):
 
 
 class UserRegistration(HTTPMethodView):
-
     async def post(self, request):
-        if not check_credentials(request):
+        user_data = check_userdata(request)
+        if user_data is None:
             return json({"success": False,
-                         "message": "No user data: user name and/or password."})
+                         "message": "No user data: username and/or password."})
 
-        if User.get(username=request.credentials.username) is None:
-            inactive_user = InactiveUser.get_or_create(username=request.credentials.username,
-                                                       password=request.credentials.password)
+        if User.get(username=user_data.username) is None:
+            inactive_user = InactiveUser.get_or_create(username=user_data.username,
+                                                       password=user_data.password)
             return json({"success": True,
                          "activation_link": f"{request.scheme}://"
                                             f"{request.host}"
@@ -44,6 +45,7 @@ class UserRegistration(HTTPMethodView):
 class GoodsList(HTTPMethodView):
 
     async def get(self, request):
+        # TODO перенести в models
         json_data = []
         goods = select(Good)
         for good in db_session.scalars(goods):
@@ -53,4 +55,17 @@ class GoodsList(HTTPMethodView):
                     "price": str(good.price)
                 }
             )
-        return json({"goods": json_data})
+        return json({"success": False,
+                     "goods": json_data})
+
+
+class Transaction(HTTPMethodView):
+    decorators = [protected()]
+
+    async def post(self, request):
+        transaction_data = check_transaction_data(request)
+        if transaction_data is None:
+            json({"success": False,
+                  "message": "Transaction is incorrect"})
+        return json({"success": True,
+                     "message": "Protected url. Transaction is correct"})
