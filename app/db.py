fac8d4ca8063e4169c6326c48815314f442f9839
Lev <Kakalashvilillev@yandex.ru>
2022-12-11 19:46:05 +0300

    refactor: не много поменял

--- app/db.py
+++ app/db.py
@@ -1,8 +1,12 @@
+"""Модуль подключения к БД."""
 from sqlalchemy import create_engine
-
-from app.settings import Config as AppConfig
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session
 
-db_engine = create_engine(AppConfig.DB_URL, echo=True if AppConfig.DEBUG == "True" else False)
+from app.settings import Config as AppConfig
+
+db_engine: Engine = create_engine(
+    AppConfig.DB_URL, echo=AppConfig.DEBUG,
+)
 
 session = Session(db_engine)
